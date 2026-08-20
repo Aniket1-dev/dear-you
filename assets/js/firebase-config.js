97b0849de051aeedf6fb4e3674e8754a8759430c
Aniket sharma <sharma790aniket@gmail.com>
2026-08-20 15:36:21 +0530

    Add Mydear Firebase configuration

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+// Mydear Firebase client configuration.
+// Client Firebase config is not a secret; protect data with Firestore Rules.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBb5w2-RMciICdijf2pOlbQ1E0wRQ6N4Qs",
+  authDomain: "zerosmoke-account.firebaseapp.com",
+  projectId: "zerosmoke-account",
+  storageBucket: "zerosmoke-account.firebasestorage.app",
+  messagingSenderId: "306579939416",
+  appId: "1:306579939416:web:f2fc1ecb3026438b3faa2d",
+  measurementId: "G-4P8WMDZ32J"
+};
+window.FIREBASE_FUNCTIONS_REGION = "us-central1";
+window.PUBLIC_APP_URL = "https://mydear.fun";
